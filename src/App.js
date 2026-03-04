dbc7083e2d71420fa71b60fab8cf78358e04f02f
ashraful <ahsan.ashraful@adndiginet.com>
2026-03-04 15:08:00 +0600

    Fetch all post from api endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const BASE_URL = 'http://127.0.0.1:8000/';
 
 function App() {
+
+  const[post, setPosts] = useState([]);
+  
+  useEffect(() => {
+    fetch(BASE_URL + 'posts/all')
+      .then(response => {
+        const json = response.json();
+        console.log(json);
+        if (response.ok) {
+          return json;
+        }  
+      })
+      .then(data => setPosts(data))
+      .catch(error => console.error('Error fetching posts:', error));
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
